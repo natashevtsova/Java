e5eafa449179aec1690c4c14ecc3a5b8720c2226
Nataliia Shevtsova <Nataliia_Shevtsova@epam.com>
2016-09-09 14:26:56 +0300

    init commit

---
+++ src/test/java/GetValueTest.java
@@ -0,0 +1,16 @@
+package ua.epam;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nataliia_Shevtsova on 9/7/2016.
+ */
+public class GetValueTest {
+    @org.junit.Test
+    public void getValueFromIndex() throws Exception {
+
+
+    }
+
+}
---
+++ src/test/java/InvertArrayTest.java
@@ -0,0 +1,22 @@
+package ua.epam;
+
+import org.junit.*;
+import org.junit.Test;
+import org.junit.Assert;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nataliia_Shevtsova on 9/7/2016.
+ */
+public class InvertArrayTest {
+        @org.junit.Test
+    public void invertArray() throws Exception {
+
+        int array [] =  {1, 2, 3, 4, 5, 6, 7, 8};
+       // int value = 5;
+            InvertArray inv= new InvertArray();
+
+
+    }
+
+}
